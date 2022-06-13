78b15b38900c7c02d7b70cf61cb672fa2635d88b
Charlie Matters <charliematters@gmail.com>
2022-06-13 09:15:25 +0100

    Fixed incorrect logging

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import { micromark } from "micromark";
 import { gfmTable } from "micromark-extension-gfm-table";
 import { beeMovieScript } from "./beeMovieScript.js";
 
-console.time("Processing with gfmTable");
+console.time("Processing without gfmTable");
 micromark(beeMovieScript, {
   extensions: [],
 });
-console.timeEnd("Processing with gfmTable");
+console.timeEnd("Processing without gfmTable");
 
-console.time("Processing without gfmTable");
+console.time("Processing with gfmTable");
 micromark(beeMovieScript, {
   extensions: [gfmTable],
 });
-console.timeEnd("Processing without gfmTable");
+console.timeEnd("Processing with gfmTable");
